8576b3b07483e77952c9de661f09d4ff307749d1
Anupam Saikia <ianupamsaikia@gmail.com>
2020-02-21 21:05:59 +0530

    removed size parameter, added user input

--- caesar.c
+++ caesar.c
@@ -11,26 +11,24 @@
  * Function to encrypt plaintext using Caesar cipher.
  * Returns the ciphertext
  */
-char *caesarEncrypt(char *plainText, int key, int size)
+char *caesarEncrypt(char *plainText, int key)
 {
+    size_t size = strlen(plainText);
     char *cipherText = (char *)malloc(sizeof(char) * size);
+
     for (int i = 0; i < size; i++)
     {
         // Uppercase will remain Uppercase; 'A'=65 'Z'=90
         if (plainText[i] >= 65 && plainText[i] <= 90)
-        {
             cipherText[i] = ((plainText[i] + key - 65) % 26) + 65;
-        }
+
         // Lowercase will remain lowercase; 'a'=97 'z'=122
         else if (plainText[i] >= 97 && plainText[i] <= 122)
-        {
             cipherText[i] = ((plainText[i] + key - 97) % 26) + 97;
-        }
+
         // Other characters will not change
         else
-        {
             cipherText[i] = plainText[i];
-        }
     }
     return cipherText;
 }
@@ -39,22 +37,57 @@ char *caesarEncrypt(char *plainText, int key, int size)
  * Function to decrypt ciphertext using Caesar cipher
  * Returns the plaintext
  */
-char *caesarDecrypt(char *cipherText, int key, int size)
+char *caesarDecrypt(char *cipherText, int key)
 {
     key = key % 26; // to make key value between [0-25]
     key = 26 - key;
-    return caesarEncrypt(cipherText, key, size);
+    return caesarEncrypt(cipherText, key);
+}
+
+// Used to clear input buffer
+void clearBuffer()
+{
+    int c = 0;
+    while ((c = getchar()) != '\n' && c != EOF)
+        ;
 }
 
 int main()
 {
-    char *str = "Sample Text?";
-    int len = strlen(str);
-    int key = 10;
+    char str[100], option = '0';
+    int key = 1;
+    printf("1. Encrypt\n2. Decrypt using key\n3. Decrypt using bruteforce\nEnter option [1-3] : ");
+    scanf("%c", &option);
 
-    printf("Text: %s\n", str);
-    printf("Encrypted: %s\n", caesarEncrypt(str, key, len));
-    printf("Decrypted: %s\n", caesarDecrypt(caesarEncrypt(str, key, len), key, len));
+    switch (option)
+    {
+    case '1':
+        printf("Enter plaintext : ");
+        clearBuffer();
+        fgets(str, 100, stdin);
+        printf("Enter key : ");
+        scanf("%d", &key);
+        printf("Ciphertext : %s", caesarEncrypt(str, key));
+        break;
+    case '2':
+        printf("Enter ciphertext : ");
+        clearBuffer();
+        fgets(str, 100, stdin);
+        printf("Enter key : ");
+        scanf("%d", &key);
+        printf("Plaintext : %s", caesarDecrypt(str, key));
+        break;
+    case '3':
+        printf("Enter ciphertext : ");
+        clearBuffer();
+        fgets(str, 100, stdin);
+        for (int i = 0; i < 26; i++)
+            printf("Using key = %d : %s", i, caesarDecrypt(str, i));
+        break;
+    default:
+        printf("Enter correct option\n");
+        break;
+    }
 
     return 0;
 }
